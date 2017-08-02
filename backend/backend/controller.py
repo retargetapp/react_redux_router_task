53e3b99e0c808eb50a22f97cef5760595e2b1240
Vova <vova.ukraine@gmail.com>
2017-08-02 23:54:12 +0300

    Refactor to JSON errors

--- backend/backend/controller.py
+++ backend/backend/controller.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.views import View
-from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
+from django.http import JsonResponse, HttpResponse
 
 import json
 import datetime
@@ -18,25 +18,25 @@ class Controller(View):
         try:
             new_event = json.loads(content)
         except Exception:
-            return HttpResponseBadRequest("Invalid json format")
+            return JsonResponse({"error": "invalid_json_format"}, status=400)
 
         if not isinstance(new_event, dict):
-            return HttpResponseBadRequest("Invalid event object fields")
+            return JsonResponse({"error": "invalid_event_object_fields"}, status=400)
 
         keys = new_event.keys()
         if 'title' not in keys or 'date' not in keys:
-            return HttpResponseBadRequest("Invalid event object fields")
+            return JsonResponse({"error": "invalid_event_object_fields"}, status=400)
 
         title = str(new_event['title'])
         date = str(new_event['date'])
 
         if title is "":
-            return HttpResponseBadRequest("Invalid title (empty)")
+            return JsonResponse({"error": "invalid_title_empty"}, status=400)
 
         try:
             datetime.datetime.strptime(date, '%Y-%m-%d')
         except ValueError:
-            return HttpResponseBadRequest('Invalid date format. Y-m-d expected')
+            return JsonResponse({"error": "invalid_date_format"}, status=400)
 
         if 'events' not in request.session:
             request.session["events"] = []
